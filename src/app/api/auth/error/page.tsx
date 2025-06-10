db21ffdab9df486107bcb40393ab3e47dd0f0417
preytech <pimonov.makar@mail.ru>
2025-06-10 08:43:55 +0300

    фикс1

--- src/app/api/auth/error/page.tsx
+++ src/app/api/auth/error/page.tsx
@@ -1,18 +1,14 @@
-"use client"
-import { useSearchParams } from "next/navigation"
+"use client";
 
-export default function Error(){
-    const searchParams = useSearchParams();
-    console.log(searchParams.get("error"))
-    return(
+export default function Error() {
+    return (
         <div className="bg-[#F9F9FA]">
             <div className="justify-between container mx-auto">
                 <div className="flex gap-2 py-72 flex-col">
                     <p className="font-Bold text-red-600 text-9xl">НАСРАЛ</p>
-                    <p className="font-Light text-black text-2xl"></p>    
+                    <p className="font-Light text-black text-2xl"></p>
                 </div>
             </div>
         </div>
-
-    )
+    );
 }
